10f5da3be6d37bfb4c1dca6aa6759dc08d28134d
Henrik Raitasola <henrikra@Henrik-MacBook-Pro.local>
2019-08-10 13:38:25 +0300

    Add custom fonts

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -11,6 +11,8 @@ import { useStaticQuery, graphql } from 'gatsby';
 
 import Header from './header';
 import './layout.css';
+import 'typeface-lato';
+import 'typeface-roboto';
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
