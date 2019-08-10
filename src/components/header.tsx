d9ebf576f456ff704b4b7fb96771af71f42a8142
Henrik Raitasola <henrikra@Henrik-MacBook-Pro.local>
2019-08-10 15:54:16 +0300

    Style navigation

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,43 +1,46 @@
-import { Link } from "gatsby"
-import PropTypes from "prop-types"
-import React from "react"
+import { Link } from 'gatsby';
+import React from 'react';
+import './header.css';
+import logo from './logo.png';
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
+type Props = {
+  siteTitle: string;
+};
+
+const Header: React.FC<Props> = ({ siteTitle }) => (
+  <div className="container">
+    <nav className="nav">
+      <div className="logo">
+        <Link to="/" className="logo-link">
+          <img src={logo} alt="Logo" className="logo-image" />
           {siteTitle}
         </Link>
-      </h1>
+      </div>
 
-    <Link to="/page-2/">Go to page 2</Link>
-    </div>
-  </header>
-)
-
-Header.propTypes = {
-  siteTitle: PropTypes.string,
-}
+      <ul className="nav-links">
+        <li className="nav-item">
+          <Link to="/" className="nav-link" activeClassName="nav-link--active">
+            Yrityksille
+            <div className="nav-link__border"></div>
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link
+            to="/page-2/"
+            className="nav-link"
+            activeClassName="nav-link--active"
+          >
+            Tekijöille
+            <div className="nav-link__border"></div>
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  </div>
+);
 
 Header.defaultProps = {
   siteTitle: ``,
-}
+};
 
-export default Header
+export default Header;
