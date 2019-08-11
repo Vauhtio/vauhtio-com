95151f6f6adaa7087600515a10bfd0d282883290
Henrik Raitasola <henrikra@Henrik-MacBook-Pro.local>
2019-08-11 13:28:48 +0300

    Add media queries for navigation

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,6 +2,7 @@ import { Link } from 'gatsby';
 import React from 'react';
 import './header.css';
 import logo from './logo.png';
+import styled from 'styled-components';
 
 type Props = {
   siteTitle: string;
@@ -11,33 +12,77 @@ const Header: React.FC<Props> = ({ siteTitle }) => (
   <div className="container">
     <nav className="nav">
       <div className="logo">
-        <Link to="/" className="logo-link">
-          <img src={logo} alt="Logo" className="logo-image" />
+        <LogoLink to="/">
+          <Logo src={logo} alt="Logo" />
           {siteTitle}
-        </Link>
+        </LogoLink>
       </div>
 
-      <ul className="nav-links">
+      <Links>
         <li className="nav-item">
-          <Link to="/" className="nav-link" activeClassName="nav-link--active">
+          <NavLink to="/" activeClassName="nav-link--active">
             Yrityksille
             <div className="nav-link__border"></div>
-          </Link>
+          </NavLink>
         </li>
         <li className="nav-item">
-          <Link
-            to="/page-2/"
-            className="nav-link"
-            activeClassName="nav-link--active"
-          >
+          <NavLink to="/page-2/" activeClassName="nav-link--active">
             Tekijöille
             <div className="nav-link__border"></div>
-          </Link>
+          </NavLink>
         </li>
-      </ul>
+      </Links>
     </nav>
   </div>
 );
+
+const LogoLink = styled(Link)`
+  font-size: 14px;
+  text-decoration: none;
+  color: #302f3c;
+  font-weight: bold;
+  display: flex;
+  align-items: center;
+
+  @media (min-width: 576px) {
+    font-size: 16px;
+  }
+`;
+
+const Logo = styled.img`
+  margin: 0;
+  margin-right: 8px;
+  width: 24px;
+
+  @media (min-width: 576px) {
+    width: auto;
+    margin-right: 16px;
+  }
+`;
+
+const Links = styled.ul`
+  display: grid;
+  grid-gap: 16px;
+  list-style: none;
+  margin: 0;
+  grid-template-columns: 1fr 1fr;
+  align-items: center;
+
+  @media (min-width: 576px) {
+    grid-gap: 48px;
+  }
+`;
+
+const NavLink = styled(Link)`
+  font-size: 14px;
+  text-decoration: none;
+  color: #302f3c;
+  position: relative;
+
+  @media (min-width: 576px) {
+    font-size: 16px;
+  }
+`;
 
 Header.defaultProps = {
   siteTitle: ``,
