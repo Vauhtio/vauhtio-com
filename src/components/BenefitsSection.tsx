7062824af88629feb59574c11850aa2541e003b4
Henrik Raitasola <henrikra@Henrik-MacBook-Pro.local>
2019-08-11 12:35:22 +0300

    Style icons of benefits

--- src/components/BenefitsSection.tsx
+++ src/components/BenefitsSection.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import styled from 'styled-components';
 
+import timer from '../pages/timer.png';
+import check from '../pages/check.png';
+import exporting from '../pages/exporting.png';
+import file from '../pages/file.png';
+
 type Benefit = {
   title: string;
   description: string;
+  icon: string;
+  iconAlt: string;
 };
 
 const benefits: Benefit[] = [
@@ -11,36 +18,47 @@ const benefits: Benefit[] = [
     title: 'Säästä aikaa ja keskity ollennaiseen',
     description:
       'Kun Vauhtio hoitaa oikeiden devaajien etsimisen ja heidän kouluttamisen timanttiselle tasolle sinulla jää aikaan omaan liiketoimintaan.',
+    icon: timer,
+    iconAlt: 'Timer clock',
   },
   {
     title: 'Kokeile ilman riskejä',
     description:
       'Koska devaajat ovat alussa konsultteina ei sinulla ole työnantajariskiä tms.',
+    icon: check,
+    iconAlt: 'Checkmark',
   },
   {
     title: 'Ulkoista koulutus',
     description:
       'Sinun ei tarvitse huolehtia oman koulutuksen tekemisestä, koska haluat keskittyä oman bisneksen kasvattamiseen.',
+    icon: exporting,
+    iconAlt: 'Arrow pointing out',
   },
   {
     title: 'Mahdollisuus palkata devaaja',
     description:
       'Konsultointijakson lopussa sinä voit päättää haluatko jatkaa samaan malliin eteenpäin vai jopa palkata devaajan.',
+    icon: file,
+    iconAlt: 'Folder with files',
   },
 ];
 
 const BenefitsSection = () => {
   return (
     <section className="benefits-section">
-      <p className="section-header__subtitle">Mitä me tuomme pöytään?</p>
+      <Subtitle>Mitä me tuomme pöytään?</Subtitle>
       <h2 className="section-header benefits__title">Näin hyödyt Vauhtiosta</h2>
       <div className="container">
         <Benefits>
           {benefits.map(benefit => (
-            <div className="benefit">
-              <h3 className="benefit__title">{benefit.title}</h3>
-              <p className="benefit__description">{benefit.description}</p>
-            </div>
+            <Container>
+              <Icon src={benefit.icon} alt="Lol" />
+              <div>
+                <Title>{benefit.title}</Title>
+                <Description>{benefit.description}</Description>
+              </div>
+            </Container>
           ))}
         </Benefits>
       </div>
@@ -48,9 +66,15 @@ const BenefitsSection = () => {
   );
 };
 
+const Subtitle = styled.p`
+  text-align: center;
+  color: #ffffff;
+`;
+
 const Benefits = styled.div`
   display: grid;
   grid-row-gap: 16px;
+  margin-top: 64px;
 
   @media (min-width: 576px) {
     grid-template-columns: 1fr 1fr;
@@ -61,6 +85,32 @@ const Benefits = styled.div`
   @media (min-width: 768px) {
     grid-column-gap: 64px;
   }
+`;
+
+const Container = styled.div`
+  background: rgba(0, 0, 0, 0.15);
+  border-radius: 4px;
+  padding: 24px;
+  display: flex;
+`;
+
+const Icon = styled.img`
+  margin: 0;
+  width: 48px;
+  height: 48px;
+  margin-right: 16px;
+`;
+
+const Title = styled.h3`
+  color: #ffffff;
+  font-size: 18px;
+  margin-top: 4px;
+  margin-bottom: 16px;
+`;
+
+const Description = styled.p`
+  color: #e6e5eb;
+  margin: 0;
 `;
 
 export default BenefitsSection;
