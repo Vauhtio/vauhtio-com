09fe7f6e28925ba50939a136cc062e07d659139a
Henrik Raitasola <henrikra@Henrik-MacBook-Pro.local>
2019-08-20 20:01:46 +0300

    Add introduction section

---
+++ src/components/IntroductionSection.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const paragraphs = [
+  'Miten lähestyä työnantajia? Kuinka olla vakuuttava työhaastattelussa? Entä mitä tapahtuu työhaastattelun jälkeen? Mitä minulta odotetaan, olenko tarpeeksi hyvä?',
+  'Me täällä Vauhtiolla tunnistetaan sun ongelma!',
+  'Usein juuri ensimmäisen työpaikan saaminen on haasteellista kun kokemusta tai näyttöjä ei ole juuri kertynyt. Olet ehkä jo hakenut muutamiin koodarin työtehtäviin tai tehnyt vapaa-ajan koodausprojekteja. Sinulla on paljon intoa koodata mutta et tiedä mitä kannattaa tehdä seuraavaksi, jotta pääset seuraavalle tasolle.',
+  'Vauhtion oma valmennusohjelma Onboarding kuljettaa sinut omatoimisesti mutta ohjatusti tehdyn koodausprojektin kautta valokeilaan, josta Vauhtion konsulttina toimimalla voit ponnistaa IT-uralle.',
+  'Vauhtion konkarit toimivat mentorinasi tarjoten kokemuksensa niin teknologiasta kuin käytännön työelämästä. Mentorin kanssa tehdään yhdessä ammattimainen työnäyte, etsitään asiakasprojekti, käydään haastattelussa, aloitetaan työt ja opitaan tekemisen kautta.',
+];
+
+const IntroductionSection = () => {
+  return (
+    <section className="introduction-section">
+      <div className="container">
+        <h2>Haluatko vauhtia hyppyysi opiskelijasta IT-ammattilaiseksi?</h2>
+        {paragraphs.map(paragraph => (
+          <p>{paragraph}</p>
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default IntroductionSection;
--- src/pages/rekry.tsx
+++ src/pages/rekry.tsx
@@ -4,10 +4,13 @@ import Layout from '../components/layout';
 import SEO from '../components/seo';
 import PageHeader from '../components/PageHeader';
 import team from '../pages/team2.jpeg';
+import IntroductionSection from '../components/IntroductionSection';
+import './rekry.css';
 
 const SecondPage = () => (
   <Layout>
     <SEO title="Rekry" />
+
     <PageHeader
       title="Tavoitteena työpaikka IT-alalla?"
       description="Olemme mentoroiva IT-konsulttitalo, jonka tavoitteena on oman koulutusohjelman kautta saada nuoria lupauksia konsulteiksi IT-alalle. Tule ohjelmaamme, jossa pääset oikeisiin koodausprojekteihin mahdollisimman nopeasti!"
@@ -15,6 +18,8 @@ const SecondPage = () => (
       ctaEmail="rekry@vauhtio.com"
       backgroundImage={team}
     />
+
+    <IntroductionSection />
   </Layout>
 );
 
