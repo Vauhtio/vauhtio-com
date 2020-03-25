a9cb6146c2f1de27b589d39a7d59609601b16626
Henrik Raitasola <henrik.raitasola@gmail.com>
2020-03-25 19:17:45 +0200

    Add Roni's testimonial

--- src/components/RekryPage/TestimonialsSection.tsx
+++ src/components/RekryPage/TestimonialsSection.tsx
@@ -2,8 +2,27 @@ import React from 'react';
 import Testimonial, { Testimonial as TestimonialType } from './Testimonial';
 import mikko from '../../images/testimonials/mikko.jpeg';
 import mahady from '../../images/testimonials/mahady.jpeg';
+import roni from '../../images/testimonials/roni.jpg';
+
+/**
+ * Roni Metsämuuronen full testimonial
+ *
+ * The onboarding with Vauhtio was everything I could have ever hoped for! During the 10 weeks of training I could feel myself improving each day, while simultaniously getting better picture of the field and what it would be like working with real professionals.
+ *
+ * With their help, I was able to land a job, a junior position in the kind of company I had desired one day to be working for, a spot I could have never reached this short in to my career on my own!
+ *
+ * This was the best thing I could have ever hoped for my professional career, and because of that I'll be in eternal debt for the guys at Vauhtio giving me a kickstart!
+ */
 
 const testimonials: TestimonialType[] = [
+  {
+    author: 'Roni Metsämuuronen',
+    workTitle: 'Junior Consultant',
+    year: 2020,
+    testimonial:
+      'With their help, I was able to land a job, a junior position in the kind of company I had desired one day to be working for, a spot I could have never reached this short in to my career on my own!',
+    avatar: roni,
+  },
   {
     author: 'Mikko Rajala',
     workTitle: 'Junior Consultant',
