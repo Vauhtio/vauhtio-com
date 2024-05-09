3d0d44b0bd9eb4d5adb2b25917d371339f594e24
Henrik Raitasola <henrik.raitasola@gmail.com>
2024-05-09 13:56:08 +0300

    Prevent search engine indexing

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -4,4 +4,12 @@
  * See: https://www.gatsbyjs.org/docs/ssr-apis/
  */
 
-// You can delete this file if you're not using it
+
+import * as React from "react";
+
+export const onRenderBody = ({ setHeadComponents }) => {
+  const NoFollow = (
+    <meta name="robots" content="noindex" />
+  );
+  setHeadComponents([NoFollow]);
+};
